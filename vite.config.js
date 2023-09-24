a673fff80a4a88f447202286112d13a393f947d0
efrainwsr <efrain.figueredowsr@gmail.com>
2023-09-24 16:48:55 -0400

    RUTA DE ICONOS EXPLICITA

--- vite.config.js
+++ vite.config.js
@@ -21,57 +21,57 @@ export default defineConfig({
         "theme_color": "#000000",
         "icons": [
           {
-            "src": "public/32.png",
+            "src": "https://menu-sable-nine.vercel.app/public/32.png",
             "sizes": "32x32",
             "type": "image/png"
           },
           {
-            "src": "public/64.png",
+            "src": "https://menu-sable-nine.vercel.app/public/64.png",
             "sizes": "64x64",
             "type": "image/png"
           },
           {
-            "src": "public/72.png",
+            "src": "https://menu-sable-nine.vercel.app/public/72.png",
             "sizes": "72x72",
             "type": "image/png"
           },
           {
-            "src": "public/96.png",
+            "src": "https://menu-sable-nine.vercel.app/public/96.png",
             "sizes": "96x96",
             "type": "image/png"
           },
           {
-            "src": "public/128.png",
+            "src": "https://menu-sable-nine.vercel.app/public/128.png",
             "sizes": "128x128",
             "type": "image/png"
           },
           {
-            "src": "public/144.png",
+            "src": "https://menu-sable-nine.vercel.app/public/144.png",
             "sizes": "144x144",
             "type": "image/png"
           },
           {
-            "src": "public/152.png",
+            "src": "https://menu-sable-nine.vercel.app/public/152.png",
             "sizes": "152x152",
             "type": "image/png"
           },
           {
-            "src": "public/192.png",
+            "src": "https://menu-sable-nine.vercel.app/public/192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "public/256.png",
+            "src": "https://menu-sable-nine.vercel.app/public/256.png",
             "sizes": "256x256",
             "type": "image/png"
           },
           {
-            "src": "public/384.png",
+            "src": "https://menu-sable-nine.vercel.app/public/384.png",
             "sizes": "384x384",
             "type": "image/png"
           },
           {
-            "src": "public/512.png",
+            "src": "https://menu-sable-nine.vercel.app/public/512.png",
             "sizes": "512x512",
             "type": "image/png"
           }
